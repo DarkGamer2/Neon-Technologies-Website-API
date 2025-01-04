b3cfb782e95941990498bd996dc6517395f6eee5
Kameer Hosein <kameer-hosien@outlook.com>
2025-01-04 13:47:10 -0400

    removed email for now

--- routes/contactRoutes.ts
+++ routes/contactRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { Request, Response } from "express";
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import Query from "../models/Query"; // Make sure this is the correct path to your model
 
@@ -20,35 +19,35 @@ router.post("/contact", async (req: Request, res: Response) => {
     const data = await query.save();
 
     // Set up nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      service: 'outlook',
-      auth: {
-        user: "nodejstester123@outlook.com",
-        pass: "Cartownhess53"
-      }
-    });
+    // const transporter = nodemailer.createTransport({
+    //   service: 'outlook',
+    //   auth: {
+    //     user: "nodejstester123@outlook.com",
+    //     pass: "Cartownhess53"
+    //   }
+    // });
 
-    // Email options
-    const mailOptions = {
-      from: 'nodejstester123@outlook.com',
-      to: process.env.DEV_EMAIL,  // Make sure DEV_EMAIL is set in your .env file
-      subject: `${query.clientFirstName} Has Requested a website/app. Details:`,
-      text: `Hello Mr.Hosein, here are the details of the client that has requested a website/app: \n
-        Student Name: ${query.clientFirstName}\n
-        Student Email: ${query.clientEmail}\n
-        Student Phone: ${query.clientPhoneNumber}\n
-        Query: ${query.clientQuery}\n
-        Best Regards`,
-    };
+    // // Email options
+    // const mailOptions = {
+    //   from: 'nodejstester123@outlook.com',
+    //   to: process.env.DEV_EMAIL,  // Make sure DEV_EMAIL is set in your .env file
+    //   subject: `${query.clientFirstName} Has Requested a website/app. Details:`,
+    //   text: `Hello Mr.Hosein, here are the details of the client that has requested a website/app: \n
+    //     Student Name: ${query.clientFirstName}\n
+    //     Student Email: ${query.clientEmail}\n
+    //     Student Phone: ${query.clientPhoneNumber}\n
+    //     Query: ${query.clientQuery}\n
+    //     Best Regards`,
+    // };
 
-    // Send email using nodemailer
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log('Error sending email:', error);
-      } else {
-        console.log('Email sent:', info.response);
-      }
-    });
+    // // Send email using nodemailer
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     console.log('Error sending email:', error);
+    //   } else {
+    //     console.log('Email sent:', info.response);
+    //   }
+    // });
 
     // Send response back to the client
     res.status(200).send(data);
